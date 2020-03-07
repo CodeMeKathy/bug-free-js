7ae04cf7b7f99956698081de64e4b8b2659e6c98
Kathy * <Kathy@KathyLambert.me>
2020-03-07 15:37:20 -0500

    Adding proxy pass thru via cors-anywhere, confirming access to recipe puppy api and adding a handleSubmit method to handle the input form submisisons

--- Ajax/CORS + Recipes/script.js
+++ Ajax/CORS + Recipes/script.js
@@ -1,12 +1,29 @@
 // Define and Establish API URL
 
-const baseEndpoint = 'https://www.recipepuppy.com/api'
+const baseEndpoint = 'http://www.recipepuppy.com/api'
+
+// Define Pass Thru Proxy
+const proxy = `https://cors-anywhere.herokuapp.com/`
+
+// Select the Form Element from the HTML and set to a variable
+const form = document.querySelector('form.search')
 
 // Create an async function to process the data fetch request
 
 async function fetchRecipes(query) {
   // Define and establish the response request
-  const response = await fetch(`${baseEndpoint}?q=${query}`)
+  const response = await fetch(`${proxy}${baseEndpoint}?q=${query}`)
   const data = await response.json()
+  console.log(data)
 }
+
+// Create a function to handle the input submit
+
+function handleSubmit(event) {
+  event.preventDefault()
+  console.log(event.currentTarget.query.value)
+}
+
+form.addEventListener('submit', handleSubmit)
+
 fetchRecipes('pizza')
