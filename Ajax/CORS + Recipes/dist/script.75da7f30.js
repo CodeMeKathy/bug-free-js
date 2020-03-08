3b5238f1845033c577a03fa26088e86a7b8d330d
Kathy * <Kathy@KathyLambert.me>
2020-03-08 18:39:25 -0400

    Creating a new displayRecipes function to map through the recipe data fetched from the API and return HTML with the recipe title, ingredients, and thumbnail if present

--- Ajax/CORS + Recipes/dist/script.75da7f30.js
+++ Ajax/CORS + Recipes/dist/script.75da7f30.js
@@ -119,7 +119,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"script.js":[function(require,module,exports) {
 // Define and Establish API URL
-const baseEndpoint = 'http://www.recipepuppy.com/api'; // Define Pass Thru Proxy
+const baseEndpoint = 'http://www.recipepuppy.com/api'; // Define Pass Thru Proxy to surpass CORS policy
 
 const proxy = `https://cors-anywhere.herokuapp.com/`; // Select the Form Element from the HTML and set to a variable
 
@@ -128,14 +128,40 @@ const form = document.querySelector('form.search'); // Create an async function 
 async function fetchRecipes(query) {
   // Define and establish the response request
   const response = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
-  const data = await response.json();
-  console.log(data);
+  const data = await response.json(); // console.log(data)
+
+  return data;
 } // Create a function to handle the input submit
 
 
-function handleSubmit(event) {
-  event.preventDefault();
-  console.log(event.currentTarget.query.value);
+async function handleSubmit(event) {
+  event.preventDefault(); // Select the event target which is the form input element
+
+  const formInputElement = event.currentTarget;
+  console.log(formInputElement.query.value); // Set form to off
+
+  formInputElement.submit.disabled = true; // Submit the search query
+
+  const recipes = await fetchRecipes(formInputElement.query.value);
+  console.log(recipes); // Reset the submit button disabled status
+
+  formInputElement.submit.disabled = false; // Run the displayRecipe Function
+
+  displayRecipes(recipes.results);
+} // Create a function to display the recipe to the DOM
+
+
+function displayRecipes(recipes) {
+  console.log('Creating HTML');
+  console.log(recipes);
+  const htmlDiv = recipes.map(recipe => {
+    return `<div>
+      <h2>${recipes.title}</h2>
+      <h5>${recipes.ingredients}</h5>
+      ${recipe.thumbnail && `<img src='${recipe.thumbnail}' alt='${recipe.title}' />`}
+    </div>`;
+  });
+  console.log(html);
 }
 
 form.addEventListener('submit', handleSubmit);
@@ -168,7 +194,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65152" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52693" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- Ajax/CORS + Recipes/script.js
+++ Ajax/CORS + Recipes/script.js
@@ -1,8 +1,7 @@
 // Define and Establish API URL
-
 const baseEndpoint = 'http://www.recipepuppy.com/api'
 
-// Define Pass Thru Proxy
+// Define Pass Thru Proxy to surpass CORS policy
 const proxy = `https://cors-anywhere.herokuapp.com/`
 
 // Select the Form Element from the HTML and set to a variable
@@ -14,14 +13,43 @@ async function fetchRecipes(query) {
   // Define and establish the response request
   const response = await fetch(`${proxy}${baseEndpoint}?q=${query}`)
   const data = await response.json()
-  console.log(data)
+  // console.log(data)
+  return data
 }
 
 // Create a function to handle the input submit
 
-function handleSubmit(event) {
+async function handleSubmit(event) {
   event.preventDefault()
-  console.log(event.currentTarget.query.value)
+  // Select the event target which is the form input element
+  const formInputElement = event.currentTarget
+  console.log(formInputElement.query.value)
+  // Set form to off
+  formInputElement.submit.disabled = true
+  // Submit the search query
+  const recipes = await fetchRecipes(formInputElement.query.value)
+  console.log(recipes)
+
+  // Reset the submit button disabled status
+  formInputElement.submit.disabled = false
+
+  // Run the displayRecipe Function
+  displayRecipes(recipes.results)
+}
+
+// Create a function to display the recipe to the DOM
+function displayRecipes(recipes) {
+  console.log('Creating HTML')
+  console.log(recipes)
+  const htmlDiv = recipes.map(recipe => {
+    return `<div>
+      <h2>${recipes.title}</h2>
+      <h5>${recipes.ingredients}</h5>
+      ${recipe.thumbnail &&
+        `<img src='${recipe.thumbnail}' alt='${recipe.title}' />`}
+    </div>`
+  })
+  console.log(html)
 }
 
 form.addEventListener('submit', handleSubmit)
